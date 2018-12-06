476b95a72ac3f36ad20336374fd2e1d16a83e9bb
Gregg Roemhildt <roemhildtg@gmail.com>
2018-12-05 22:23:36 -0600

    remove icons

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,24 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { ArcGISProvider } from './ArcGISProvider';
+import { ArcGISTreeProvider } from './ArcGISTreeProvider';
+import copy from './util/copy';
+import refresh from './util/refresh';
+import open from './util/open';
+import { ArcGISDocumentProvider } from './ArcGISDocumentProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     
-    const arcgisProvider = new ArcGISProvider();
-    vscode.window.registerTreeDataProvider('arcgisAssistant', arcgisProvider);
-    vscode.commands.registerCommand('arcgisAssistant.refreshEntry', () => vscode.window.showInformationMessage('Refresh', 'Test'));
+    const arcgisTreeProvider = new ArcGISTreeProvider(context);
+    const arcgisDocumentProvider = new ArcGISDocumentProvider();
+    vscode.workspace.registerTextDocumentContentProvider(ArcGISDocumentProvider.scheme, arcgisDocumentProvider);
+    
+    vscode.window.registerTreeDataProvider('arcgisAssistant', arcgisTreeProvider);
+    vscode.commands.registerCommand('arcgisAssistant.refreshEntry', refresh);
+    vscode.commands.registerCommand('arcgisAssistant.copy', copy);
+    vscode.commands.registerCommand('arcgisAssistant.open', open);
 }
 
 // this method is called when your extension is deactivated
