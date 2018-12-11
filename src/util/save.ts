96d662b083763ae390e338002052e321ee6f12dd
Gregg Roemhildt <roemhildtg@gmail.com>
2018-12-10 22:22:17 -0600

    save is working

--- src/util/save.ts
+++ src/util/save.ts
@@ -2,7 +2,25 @@ import * as path from 'path';
 import * as fs from 'fs';
 import axios from 'axios';
 import * as qs from 'query-string';
+import * as FormData from 'form-data';
+
 import {token} from './token';
+
+function serialize(obj, prefix) {
+    let str = [];
+    for (let p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            let k = prefix ? prefix + "[" + p + "]" : p;
+            let v = obj[p];
+            str.push(typeof v === "object" ?
+                serialize(v, k) :
+                encodeURIComponent(k) + "=" + encodeURIComponent(v));
+        }
+    }
+    return str.join("&");
+}
+
+
 export default function save(filePath : string){
     const parts = filePath.split(path.sep);
     let id  = parts.pop() || '';
@@ -13,18 +31,21 @@ export default function save(filePath : string){
     const folder = parts.pop();
     const username = parts.pop();
     const portal = parts.pop();
+    const url = `https://${portal}/sharing/rest/content/users/${username}/${folder}/items/${id}/update`;
 
-    axios.post(`https://${portal}/sharing/rest/content/users/${username}/${folder}/items/${id}/update`, {
-        text: encodeURIComponent(json),
+    const payload = {
+        text: JSON.stringify(json),
         f: 'json',
         token,
-    }, {
+    };
+    axios.post(url, serialize(payload), {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     }).catch(e => {
         console.warn(e);
     }).then(result => {
+        console.log(result);
     });
     
 }
