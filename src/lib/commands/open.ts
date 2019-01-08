30e2f2d93e0961f84c7a56ac9b5ea32c169cc2c1
Gregg Roemhildt <roemhildtg@gmail.com>
2019-01-08 16:46:31 -0600

    fixup open files

--- src/lib/commands/open.ts
+++ src/lib/commands/open.ts
@@ -5,17 +5,17 @@ export default async function(item :ArcGISItem, scope : any){
     let data = await item.connection.getItem(item.id);
     const directory = `memfs:/${item.connection.url}`;
     const folder = item.folder && item.folder.type === ArcGISType.Folder ? 
-        `${directory}/${item.folder.id}` : undefined;
-    const file = folder ? `${directory}/${folder}/${item.id}.json`
-        : `${directory}/${folder}/${item.id}.json`;
+        item.folder.id : undefined;
+    const path = folder ? `${directory}/${folder}/${item.id}.json`
+        : `${directory}/${item.id}.json`;
     scope.fs.createDirectory(Uri.parse(directory));
     if(folder){
-        scope.fs.createDirectory(folder);
+        scope.fs.createDirectory(Uri.parse(`${directory}/${folder}`));
     }
-    scope.fs.writeFile(Uri.parse(file), Buffer.from(data), { 
+    scope.fs.writeFile(Uri.parse(path), Buffer.from(data), { 
         create: true, overwrite: true 
     });
-    workspace.openTextDocument(Uri.parse(file)).then(doc => {
+    workspace.openTextDocument(Uri.parse(path)).then(doc => {
         window.showTextDocument(doc);
         commands.executeCommand('editor.action.format');
     }, (e: any) => console.warn(e));
