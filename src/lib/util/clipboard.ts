4d49e6921a218c0157af2f6fca735fc0ad948e76
roemhildtg <roemhildtg@gmail.com>
2019-12-20 22:15:44 -0600

    use vscode's builtin clipboard

--- src/lib/util/clipboard.ts
+++ src/lib/util/clipboard.ts
@@ -1,25 +1,11 @@
 import {copy as c, paste as p} from 'copy-paste'
 
-export function copy(data : any) : Promise<any> {
-    return new Promise((resolve, reject) => {
-        c(data, (err)  => {
-            if(err){
-                reject(err)
-            } else {
-                resolve();
-            }
-        });
-    });
+import {env} from 'vscode';
+
+export function copy(data : string) : Thenable<void> {
+    return env.clipboard.writeText(data);
 }
 
-export function paste() : Promise<any> {
-    return new Promise((resolve, reject) => {
-        p((err, content) => {
-            if(err){
-                reject(err);
-            } else {
-                resolve(content);
-            }
-        });
-    });
+export function paste() : Thenable<string> {
+    return env.clipboard.readText();
 }
